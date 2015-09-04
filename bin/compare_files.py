e9adacd3ffedf6ad9c78da89283db194e98fe375
Matthew Vincent <matt.vincent@jax.org>
2015-09-04 10:59:29 -0400

    Initial commit

---
+++ bin/compare_files.py
@@ -0,0 +1,58 @@
+import os
+import subprocess
+import sys
+
+import pysam
+
+def write_header(header, out):
+    out.write(">")
+    out.write(header)
+    out.write("\n")
+
+def write_sequence(sequence, out):
+    for i in xrange(0, len(sequence), 60):
+        out.write(sequence[i:i+60])
+        out.write("\n")
+
+def muscle_it(fasta_file):
+    subprocess.call(['muscle', '-clw', '-in', fasta_file])
+
+def diff_files(file1, file2):
+
+    fasta_1 = pysam.FastaFile(file1)
+    fasta_2 = pysam.FastaFile(file2)
+
+    muscle_file = "muscle.fa"
+    mf = open(muscle_file, "w")
+
+    fa_equal = {}
+    fa_diff = {}
+
+    for seq_id1 in fasta_1.references:
+        if seq_id1 in fasta_2.references:
+            print "Comparing {}".format(seq_id1)
+
+            if fasta_1.fetch(seq_id1) == fasta_2.fetch(seq_id1):
+                fa_equal[seq_id1] = seq_id1
+            else:
+                fa_diff[seq_id1] = seq_id1
+
+                write_header(seq_id1 + "_1", mf)
+                write_sequence(fasta_1.fetch(seq_id1), mf)
+
+                write_header(seq_id1 + "_2", mf)
+                write_sequence(fasta_2.fetch(seq_id1), mf)
+
+    mf.close()
+
+    print "# EQUAL: {0}".format(len(fa_equal))
+    print "# DIFF: {0}".format(len(fa_diff))
+
+    if len(fa_diff) > 0:
+        muscle_it(muscle_file)
+        os.remove(muscle_file)
+
+if __name__ == '__main__':
+    diff_files(sys.argv[1], sys.argv[2])
+
+
